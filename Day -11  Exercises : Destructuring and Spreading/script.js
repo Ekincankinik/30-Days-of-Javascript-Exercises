bcc8022fd785006b6eaa30b7643d759deadbb0e4
Ekin <147428702+Ekincankinik@users.noreply.github.com>
2024-03-18 23:34:47 +0300

    Update script.js

--- Day -11  Exercises : Destructuring and Spreading/script.js
+++ Day -11  Exercises : Destructuring and Spreading/script.js
@@ -142,3 +142,27 @@ console.log(object)
 }}
 
 convertArrayToObject(students)
+
+
+//  4. Copy the student object to newStudent without mutating the original object. In the new      object add the following ?
+//   Add Bootstrap with level 8 to the front end skill sets
+//   Add Express with level 9 to the back end skill sets
+//   Add SQL with level 8 to the data base skill sets
+//   Add SQL without level to the data science skill sets
+
+
+
+  const newStudent = {...student}
+
+  let newSkill = {skill: 'Bootstrap', level : 8}
+  let newBackEndSkill = {skill:'Express', level: 9}
+  let newDataSkill = {skill: 'SQL', level: 8}
+  let newDataScienceSkill = 'SQL'
+
+  newStudent.skills.backEnd.push(newBackEndSkill)
+  newStudent.skills.dataBase.push(newDataSkill)
+  newStudent.skills.dataScience.push(newDataScienceSkill)
+  newStudent.skills.frontEnd.push(newSkill)
+
+  console.log(newStudent)
+  
